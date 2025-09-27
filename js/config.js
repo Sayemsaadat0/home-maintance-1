ec018c8c2956603c3eb9ed514df6f2bfd7136ef7
Sayemsaadat0 <sayemsaadat0@gmail.com>
2025-09-28 01:25:28 +0600

    changes

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// const ENV = {
+//     EMAILJS_SERVICE_ID: "service_tl0jx2r",
+//     EMAILJS_TEMPLATE_ID: "template_cl0w6da", 
+//     EMAILJS_PUBLIC_KEY: "7w9v4-lXumnMGOS-1"
+//   };
+  
+const ENV = {
+    EMAILJS_SERVICE_ID: "service_rwrqe24",
+    EMAILJS_TEMPLATE_ID: "template_h8mg5fc", 
+    EMAILJS_PUBLIC_KEY: "wJEb2VKHBqBiwL10y"
+  };
+  
